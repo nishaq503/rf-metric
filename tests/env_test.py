9b227b4be259f1972bfaccc21281633d8de6c9cd
Najib Ishaq <najib_ishaq@zoho.com>
2025-07-11 17:14:30 -0400

    feat: drawing animations of rf-data in jupyter notebook

--- tests/env_test.py
+++ tests/env_test.py
@@ -2,7 +2,7 @@
 
 import pathlib
 
-from rf_metric.utils import config
+from rf_metric.utils import config  # type: ignore[import-not-found]
 
 
 def test_config() -> None:
@@ -10,8 +10,13 @@ def test_config() -> None:
     assert len(config) > 0, "Configuration should not be empty"
     assert "RF_DATA_DIR" in config, "RF_DATA_DIR should be in the configuration"
 
-    rf_data_dir = pathlib.Path(config["RF_DATA_DIR"]).resolve()  # type: ignore
+    rf_data_dir = pathlib.Path(config["RF_DATA_DIR"]).resolve()  # pyright: ignore[reportArgumentType]
     assert rf_data_dir.exists(), f"RF data directory {rf_data_dir} should exist"
     assert rf_data_dir.is_dir(), (
         f"RF data directory {rf_data_dir} should be a directory"
+    )
+
+    assert "LOG_LEVEL" in config, "LOG_LEVEL should be in the configuration"
+    assert config["LOG_LEVEL"] in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], (
+        f"LOG_LEVEL should be one of DEBUG, INFO, WARNING, ERROR, CRITICAL, but got {config['LOG_LEVEL']}"
     )
